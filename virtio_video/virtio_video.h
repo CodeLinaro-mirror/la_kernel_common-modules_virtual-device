66f6fbeb362ac5b6a62a7cb52315fdbecc34b602
Darek Stojaczyk <dstojacz@qti.qualcomm.com>
2024-04-05 16:40:06 +0200

    virtio_video: Move most of virtio_video_enum_framesizes() into a helper function.
    virtio_video_enum_framesizes() is about to be implemented
    differently in encoder and decoder, so factor out the common
    logic into a public helper function. No functional changes.
    
    Issue: COQOS-15787
    
    [ovolkov@qti.qualcomm.com: Cherry-picked and refined commit message from:
    repository: ssh://git@stash.opensynergy.com:7999/coqos-virtio/coqos-virtio-video-driver-linux
    branch: master
    commit: a1b40116cc7]
    
    Issue: COQOS-18777
    Change-Id: I9981cdc8b32fa6ae87b75809ffc3e61532af4214
    SrcRevId: 05444063624d9ab7794a7cd22498ea0e1c190f15
    (cherry picked from commit 50bf2595ec3d83760b4e43f326e618346e7934ce)
    Signed-off-by: Houffaneh Osman <hosman@qti.qualcomm.com>

--- virtio_video/virtio_video.h
+++ virtio_video/virtio_video.h
@@ -434,6 +434,8 @@ void virtio_video_pix_fmt_sp2mp(const struct v4l2_pix_format *pix,
 				struct v4l2_pix_format_mplane *pix_mp);
 void virtio_video_pix_fmt_mp2sp(const struct v4l2_pix_format_mplane *pix_mp,
 				struct v4l2_pix_format *pix);
+int virtio_video_frmsizeenum_from_fmt(struct video_format *fmt,
+				      struct v4l2_frmsizeenum *f);
 
 int virtio_video_g_selection(struct file *file, void *fh,
 			     struct v4l2_selection *sel);
